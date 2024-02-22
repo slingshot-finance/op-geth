ae55d6b63a6629a36d1d09e8d849a97140075e42
De'on Summers <deonsummers91@gmail.com>
2024-02-22 09:37:47 -0600

    update c bindings

--- crypto/secp256k1/panic_cb.go
+++ crypto/secp256k1/panic_cb.go
@@ -13,12 +13,12 @@ import "unsafe"
 // Callbacks for converting libsecp256k1 internal faults into
 // recoverable Go panics.
 
-//export secp256k1GoPanicIllegal
-func secp256k1GoPanicIllegal(msg *C.char, data unsafe.Pointer) {
+//export secp256k1GoPanicIllegalOP
+func secp256k1GoPanicIllegalOP(msg *C.char, data unsafe.Pointer) {
 	panic("illegal argument: " + C.GoString(msg))
 }
 
-//export secp256k1GoPanicError
-func secp256k1GoPanicError(msg *C.char, data unsafe.Pointer) {
+//export secp256k1GoPanicErrorOP
+func secp256k1GoPanicErrorOP(msg *C.char, data unsafe.Pointer) {
 	panic("internal error: " + C.GoString(msg))
 }
